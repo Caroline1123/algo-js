516b9fd1316bb3beeb625fa07c3b034b2ba6b1fa
Caroline <deconinck.caro@gmail.com>
2024-04-03 10:21:57 +0200

    Added extra challenges and resolution

--- extra_challenges/Manhattan/app.js
+++ extra_challenges/Manhattan/app.js
@@ -1,3 +1,10 @@
 (() => {
+    // Creates function that takes 2 params : 
+    // coordinates of street/aveA and street/aveB and calculates the Manhattan distance between those 2.
 
+    let manhattan = (streetA, avenueA, streetB, avenueB) => {
+        return Math.abs(streetA - streetB) + Math.abs(avenueA - avenueB);
+    }
+
+    console.log(manhattan(1, 1, 2, 2));
 })();
--- extra_challenges/change_calculator/app.js
+++ extra_challenges/change_calculator/app.js
@@ -1,17 +1,44 @@
 (() => {
     // Create function taking the price and the total received as parameters. This returns an array containing the minimum amount of bank notes/coins to be handed.
+
+    // Create a function that checks how many times an amount is still in the remaining amount , and update toBeHanded array.
+    function checkHowMany(remaining, amount, toBeHanded) {
+        if (remaining >= amount) {
+            let nb_quotes = Math.floor(remaining/amount);
+            for (nb_quotes ; nb_quotes > 0; nb_quotes--) {
+                toBeHanded.push(amount);
+                remaining -= amount;
+                remaining = remaining.toFixed(2)
+            }
+        }
+        return remaining
+    }
+
     function computeChange(price, moneyHanded){
         // calculate amount to be returned 
-        let difference = price - moneyHanded;
+        let remaining = moneyHanded - price;
         let toBeHanded = []
-        if (difference >= 500) {
-            let count_notes_500 = Math.floor(difference/500)
-            for (count of count_notes_500) {
-                toBeHanded.push(500)
-                difference = 
+
+        // Checks if parameters are valid, if not, return empty array
+        if (!price || !moneyHanded || typeof(price)!= "number" || typeof(moneyHanded) != "number" || price > moneyHanded) {
+            return toBeHanded;
+        }
+        if (remaining == 0) {
+            return toBeHanded;
+        }
+
+        currency = [500.00, 200.00, 100.00, 50.00, 20.00, 10.00, 5.00, 2.00, 1.00, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01]
+
+        for (let i = 0; i < currency.length ; i ++ ) {
+            remaining = checkHowMany(remaining, currency[i], toBeHanded)
+            if(remaining == 0 ) {
+                break;
             }
         }
 
-      }
+        return toBeHanded
+    }
+
+    console.log(computeChange(12.3, 50));
 
 })();
